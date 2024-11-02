29c1c66ddf0a7badd691ae7283bbc9b380aa9ac8
SantaSpeen <santaspeen@yandex.ru>
2024-11-02 18:52:35 +0300

    [~] Read settings from env

--- src/client/main.py
+++ src/client/main.py
@@ -1,6 +1,7 @@
 import os
 import platform
 import socket
+import sys
 import time
 from datetime import datetime, timezone, timedelta
 from pathlib import Path
@@ -8,6 +9,14 @@ from pathlib import Path
 import psutil
 import requests
 
+ENDPOINT = os.getenv("ENDPOINT", "http://127.0.0.1:5000/client")
+HASH_FILE = os.getenv("HASH_FILE", "device.hash")
+LOG_FILE = os.getenv("LOG_FILE", "killer-client.txt")
+KILLER_APP = os.getenv("KILLER_APP", "0")
+
+app = False
+if KILLER_APP == "1":
+    app = True
 
 def get_ip_mac_addresses():
     interfaces = psutil.net_if_addrs()
@@ -28,27 +37,28 @@ def get_ip_mac_addresses():
     print("Found interfaces:", ifaces)
     return ifaces
 
-def shutdown():
+def shutdown(log_file=LOG_FILE):
     operating_system = platform.system()
     if operating_system == "Windows":
-        with open("killer-client.txt", "a") as f:
+        with open(log_file, "a") as f:
             f.write(f"{datetime.now()} Shutdown request from killer server.\n")
-        os.system("shutdown /s /t 1")
+        # os.system("shutdown /s /t 1")
     else:
-        with open("/var/log/killer-client", "a") as f:
+        with open(log_file, "a") as f:
             f.write(f"{datetime.now()} Shutdown request from killer server.")
-        os.system("shutdown -P now")
+        # os.system("shutdown -P now")
+    sys.exit(0)
 
 
 class Host:
     ping_interval = timedelta(minutes=1)
     update_interval = timedelta(hours=12)
 
-    def __init__(self, endpoint, save_dir):
+    def __init__(self, endpoint, hash_file):
         self.run = False
 
         self.endpoint = endpoint
-        self.save_dir = Path(save_dir)
+        self.hash_file = Path(hash_file)
         self.session = requests.Session()
 
         self.ips = []
@@ -59,7 +69,6 @@ class Host:
         self.last_update = None
         self.device_hash = None
 
-
     def _update_params(self):
         self.hostname = socket.gethostname()
         ifaces = get_ip_mac_addresses()
@@ -67,18 +76,16 @@ class Host:
         self.macs = [mac for _, mac in ifaces]
 
     def _save_hash(self):
-        if not self.save_dir.exists():
-            self.save_dir.mkdir()
         if self.last_update is None or self.device_hash is None:
             print("Can't save hash: last_update or device_hash is None")
             return
-        with open(self.save_dir / "device.hash", "w") as f:
+        with open(self.hash_file, "w") as f:
             f.write(f"{self.last_update.timestamp()}::{self.device_hash}")
 
     def _read_hash(self):
-        if not (self.save_dir / "device.hash").exists():
+        if not self.hash_file.exists():
             return
-        with open(self.save_dir / "device.hash", "r") as f:
+        with open(self.hash_file, "r") as f:
             d = f.read().strip()
         last_update, self.device_hash = d.split("::", 1)
         self.last_update = datetime.fromtimestamp(float(last_update), timezone.utc)
@@ -140,6 +147,7 @@ class Host:
         if self.api("ping").get("message") == "pong":
             print("Connected to server")
             self.run = True
+
     def start(self):
         print(f"Ping interval: {self.ping_interval}; Update interval: {self.update_interval};")
         self._read_hash()
@@ -151,14 +159,23 @@ class Host:
             if p.get("code") == 4:
                 print('wtf')
                 self._pre_start()
-            if p.get("kill"):
-                shutdown()
+
+            if p.get("kill_apps"):
+                if app:
+                    print("Received kill_apps request. Shutting down...")
+                    shutdown(LOG_FILE)
+                else:
+                    print("Received kill_apps request, but client registered not as app-host. Ignoring...")
+            if p.get("kill_other"):
+                print("Received kill_other request. Shutting down...")
+                shutdown(LOG_FILE)
+
             time.sleep(self.ping_interval.total_seconds())
 
 
 if __name__ == '__main__':
     print("Starting client...")
-    host = Host("http://127.0.0.1:5000/client", "./")
+    host = Host(ENDPOINT, HASH_FILE)
     try:
         host.start()
     except KeyboardInterrupt:
